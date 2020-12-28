9746a00a719b5136d8777b8a7229605a83dd6fff
Ziggy_PC <ziggys_42@hotmail.com>
2020-12-28 11:02:30 +0000

    updated tb for nandn to use procedures

--- 01 Boolean Logic/tb_nandn.vhd
+++ 01 Boolean Logic/tb_nandn.vhd
@@ -14,7 +14,7 @@
 --------------------------------------------------------------------------------
 -- Revisions  :
 -- Date        Version  Author  Description
--- 2020-12-27  1.0      TZS     Created
+-- 2020-12-28  1.0      TZS     Created
 --------------------------------------------------------------------------------
 LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
@@ -48,7 +48,7 @@ ARCHITECTURE tb OF tb_nandn IS
   TYPE WidthArray IS ARRAY(test_case_count_g - 1 DOWNTO 0) OF 
                                      INTEGER RANGE max_width_g - 1 DOWNTO 0;
   TYPE ValArray IS ARRAY(test_case_count_g - 1 DOWNTO 0) OF 
-                            SIGNED(2**(test_case_count_g - 1) - 1 DOWNTO 0); 
+                          UNSIGNED(2**(test_case_count_g - 1) - 1 DOWNTO 0); 
   
   -- array containing widths of each component instance within the tb
   SIGNAL width_vals                       : WidthArray := (OTHERS => 0);
@@ -57,6 +57,7 @@ ARCHITECTURE tb OF tb_nandn IS
   SIGNAL a_in_vals, b_in_vals, a_out_vals : ValArray;
 
 BEGIN -- architecture
+
   -- generate instances of device under verification, number controlled by 
   -- generic 'test_case_count_g'
   generate_duv : FOR i IN test_case_count_g - 1 DOWNTO 0 GENERATE
@@ -68,13 +69,41 @@ BEGIN -- architecture
       PORT MAP (
                  a_in => STD_LOGIC_VECTOR(a_in_vals(i)((2**i) - 1 DOWNTO 0)),
                  b_in => STD_LOGIC_VECTOR(b_in_vals(i)((2**i) - 1 DOWNTO 0)),
-                 SIGNED(a_out) => a_out_vals(i)((2**i) - 1 DOWNTO 0)
+                 UNSIGNED(a_out) => a_out_vals(i)((2**i) - 1 DOWNTO 0)
                );
 
   END GENERATE generate_duv;
 
   test : PROCESS IS -- process to run through testing logic
-  BEGIN
+    
+  -- procedure for asserting outputs are correct
+    PROCEDURE checkVals IS 
+    BEGIN
+      -- Assert NAND of inputs and outputs is correct
+      FOR tc IN test_case_count_g - 1 DOWNTO 0 LOOP
+        FOR k IN width_vals(tc) - 1 DOWNTO 0 LOOP
+
+          ASSERT a_out_vals(tc)(k) = (a_in_vals(tc)(k) NAND b_in_vals(tc)(k))
+            REPORT "NAND result does not match expected value."
+            SEVERITY FAILURE;
+
+        END LOOP;
+      END LOOP;
+    END PROCEDURE checkVals;
+
+    -- procedure used to flip bits within ValArray
+    PROCEDURE flipBits (SIGNAL val_array : INOUT ValArray) IS
+    BEGIN 
+
+      test_case_loop : FOR tc IN test_case_count_g - 1 DOWNTO 0 LOOP
+          
+          val_array(tc) <= val_array(tc) + ((2**width_vals(tc)) - 1);
+          WAIT FOR 1 NS;
+
+      END LOOP test_case_loop;
+    END PROCEDURE flipBits;
+
+  BEGIN -- test process
 
     ASSERT (width_vals(test_case_count_g - 1) <= max_width_g)
       REPORT "Max width value is exceeded!"
@@ -93,39 +122,13 @@ BEGIN -- architecture
       WAIT FOR 1 NS;
 
     ELSE
-      -- increment input a vals and verify the output is correct
-      test_case_loop_a : FOR i IN test_case_count_g - 1 DOWNTO 0 LOOP
-       bit_loop_a : FOR j IN (2**width_vals(i)) - 2 DOWNTO 0 LOOP
+      
+      checkVals; -- assert output is correct
+      flipBits(a_in_vals); -- flip bits in a
+      checkVals; -- assert output is correct
 
-          a_in_vals(i) <= a_in_vals(i) + 1;
-          WAIT FOR 1 NS;
-
-          check_output_a : FOR k IN width_vals(i) - 1 DOWNTO 0 LOOP
-
-            ASSERT a_out_vals(i)(k) = (a_in_vals(i)(k) NAND b_in_vals(i)(k))
-              REPORT "NAND result does not match expected value."
-              SEVERITY FAILURE;
-
-          END LOOP check_output_a;
-        END LOOP bit_loop_a;
-      END LOOP test_case_loop_a;
-
-      -- increment input b vals and verify the output is correct
-      test_case_loop_b : FOR i IN test_case_count_g - 1 DOWNTO 0 LOOP 
-      bit_loop_b : FOR j IN (2**width_vals(i)) - 2 DOWNTO 0 LOOP
-          
-          b_in_vals(i) <= b_in_vals(i) + 1;
-          WAIT FOR 1 NS;
-
-          check_output_b : FOR k IN width_vals(i) - 1 DOWNTO 0 LOOP
-
-            ASSERT a_out_vals(i)(k) = (a_in_vals(i)(k) NAND b_in_vals(i)(k))
-              REPORT "NAND result does not match expected value."
-              SEVERITY FAILURE;
-
-          END LOOP check_output_b;
-        END LOOP bit_loop_b;
-      END LOOP test_case_loop_b;
+      flipBits(b_in_vals); -- flip bits in b
+      checkVals; -- assert output is correct
     
       ASSERT FALSE -- end test
         REPORT "Simulation successful!."
